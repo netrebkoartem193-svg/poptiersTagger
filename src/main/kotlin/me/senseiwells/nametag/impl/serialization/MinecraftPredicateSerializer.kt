7951204b1bbb9d39abe1a36dba772f49bc2fdfea
netrebkoartem193-svg <netrebkoartem193@gmail.com>
2026-08-24 18:34:14 +0300

    Refactor MinecraftPredicateSerializer for serialization

--- src/main/kotlin/me/senseiwells/nametag/impl/serialization/MinecraftPredicateSerializer.kt
+++ src/main/kotlin/me/senseiwells/nametag/impl/serialization/MinecraftPredicateSerializer.kt
@@ -1,40 +1,21 @@
 package me.senseiwells.nametag.impl.serialization
 
-import com.google.gson.GsonBuilder
-import eu.pb4.predicate.api.GsonPredicateSerializer
-import eu.pb4.predicate.api.MinecraftPredicate
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.JsonDecoder
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonEncoder
-import net.minecraft.core.RegistryAccess
-import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.util.Identifier
+import me.senseiwells.nametag.impl.predicate.MinecraftPredicate
 
-object MinecraftPredicateSerializer: KSerializer<MinecraftPredicate> {
-    private val GSON = GsonBuilder()
-        .disableHtmlEscaping()
-        .registerTypeHierarchyAdapter(MinecraftPredicate::class.java, GsonPredicateSerializer.create(RegistryAccess.fromRegistryOfRegistries(BuiltInRegistries.REGISTRY)))
-        .create()
-
-    override val descriptor: SerialDescriptor = JsonElement.serializer().descriptor
-
-    override fun deserialize(decoder: Decoder): MinecraftPredicate {
-        if (decoder !is JsonDecoder) {
-            throw IllegalArgumentException("Can only deserialize MinecraftPredicates as JSON")
-        }
-
-        return GSON.fromJson(decoder.json.encodeToString(decoder.decodeJsonElement()), MinecraftPredicate::class.java)
-    }
+object MinecraftPredicateSerializer : KSerializer<MinecraftPredicate> {
+    override val descriptor: SerialDescriptor = ResourceLocationSerializer.descriptor
 
     override fun serialize(encoder: Encoder, value: MinecraftPredicate) {
-        if (encoder !is JsonEncoder) {
-            throw IllegalArgumentException("Can only deserialize MinecraftPredicates as JSON")
-        }
+        ResourceLocationSerializer.serialize(encoder, value.id)
+    }
 
-        encoder.encodeJsonElement(encoder.json.parseToJsonElement(GSON.toJson(value)))
+    override fun deserialize(decoder: Decoder): MinecraftPredicate {
+        val id = ResourceLocationSerializer.deserialize(decoder)
+        return MinecraftPredicate.get(id) ?: throw IllegalArgumentException("Unknown predicate id: $id")
     }
 }
